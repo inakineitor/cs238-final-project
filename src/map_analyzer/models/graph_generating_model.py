3d96fce81ef623cc9154b258b35bba913a0dafb5
jeffreywang <wang.g.jeffrey@gmail.com>
2024-04-13 15:33:09 -0400

    Small changes - neutralize radius test for now + optinal graph gen params

--- src/map_analyzer/models/graph_generating_model.py
+++ src/map_analyzer/models/graph_generating_model.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from typing import Optional, Union
+from typing import Optional, Union, Any
 
 from gerrychain import Graph
 from numpy.random import Generator
@@ -12,7 +12,7 @@ class GraphGeneratingModel(ABC):
         pass
 
     def generate_graphs(
-        self, num_graphs: int, seed: Optional[int] = None
+        self, num_graphs: int, seed: Optional[int] = None, opt_params: Optional[dict[str, Any]] = None
     ) -> list[Graph]:
         rng = np.random.default_rng(seed)
         return [self.generate_graph(seed=rng) for _ in range(num_graphs)]
