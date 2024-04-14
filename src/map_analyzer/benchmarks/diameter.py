612df6db0b784f000f064464cdbc7a35fcbca0b3
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2024-04-14 15:39:16 -0400

    Small changes to radius and diameter

--- src/map_analyzer/benchmarks/diameter.py
+++ src/map_analyzer/benchmarks/diameter.py
@@ -5,5 +5,5 @@ from ..benchmark_framework.benchmark import Benchmark
 
 class DiameterBenchmark(Benchmark):
     def score_graph(self, graph) -> float:
-       # return 1
-       return nx.diameter(graph) # NOTE - This won't work for non-connected graphs
+       # Note: diameter isn't defined for disconnected graphs
+       return nx.diameter(graph) if nx.is_connected(graph) else -1
--- src/map_analyzer/benchmarks/radius.py
+++ src/map_analyzer/benchmarks/radius.py
@@ -5,5 +5,5 @@ from ..benchmark_framework.benchmark import Benchmark
 
 class RadiusBenchmark(Benchmark):
     def score_graph(self, graph) -> float:
-       # return 1
-       return nx.radius(graph) # NOTE - This won't work for non-connected graphs
+       # Note: radius isn't defined for disconnected graphs
+       return nx.radius(graph) if nx.is_connected(graph) else -1
